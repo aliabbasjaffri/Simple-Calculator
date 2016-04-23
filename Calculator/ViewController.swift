0aad6497cf13e4c0dcba6847c18fac83f2d8f14b
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-04-23 15:38:03 +0500

    Setting up Calculator UI. Buttons and Display

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func touchDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        print("touched \(digit) digit" )
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
