cfeb52d562cd5d90b4b539d89a1d29bc2cd61b27
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-04-23 23:47:38 +0500

    Edits to Calculator Brain.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -12,6 +12,21 @@ class CalculatorBrain
 {
     var accumulator = 0.0
     
+    var operations : Dictionary<String , Operations> = [
+        "π" : Operations.Constant(M_PI),
+        "e" : Operations.Constant(M_E),
+        "cos" : Operations.UnaryOperation,
+        "√" : Operations.UnaryOperation
+        ]
+    
+    enum Operations
+    {
+        case Constant(Double)
+        case UnaryOperation
+        case BinaryOperation
+        case Equals
+    }
+    
     func setOperand( operand: Double )
     {
         accumulator = operand
@@ -19,7 +34,16 @@ class CalculatorBrain
     
     func performOperation( symbol : String )
     {
-        
+        if let operation = operations[symbol]
+        {
+            switch operation
+            {
+            case .Constant(let value) : accumulator = value
+            case .BinaryOperation : break
+            case .UnaryOperation : break
+            case .Equals : break
+            }
+        }
     }
     
     var result : Double{
