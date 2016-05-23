94603fec2393f77712f86f67ebdfa2ca1dde0280
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-05-23 23:43:12 +0500

    Graph view to contain AxesDrawer

---
+++ Calculator/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Calculator
+//
+//  Created by Ali Abbas Jaffri on 23/05/2016.
+//  Copyright © 2016 Ali Abbas Jaffri. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
