7982346ca981593c8d65bd6d229fafeb5040e298
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2016-05-24 23:47:49 +0500

    Graph Axes drawn

--- Calculator/GraphView.swift
+++ Calculator/GraphView.swift
@@ -8,14 +8,70 @@
 
 import UIKit
 
-class GraphView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+@IBDesignable
+class GraphView: UIView
+{
+    var lineWidth : CGFloat = 1.0 { didSet{setNeedsDisplay()} }
+    var lineColor : UIColor = UIColor.blueColor() { didSet{setNeedsDisplay()} }
+    var scale: CGFloat = 50.0 {
+        didSet {
+            updateRange()
+            setNeedsDisplay()
+        }
     }
-    */
+    
+    private var rangeMinimumX : CGFloat = -50.0
+    private var rangeMaximumX : CGFloat = 50.0
+    
+    private var rangeMinimumY : CGFloat = -25.0
+    private var rangeMaximumY : CGFloat = 25.0
+    
+    private var increment : CGFloat = 1.0
+    
+    private var viewCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
+    }
+    
+    private var axesOrigin: CGPoint = CGPoint(x: 0, y:0 ) {
+        didSet {
+            updateRange()
+            setNeedsDisplay()
+        }
+    }
+    
+    // viewBounds passed to AxesDrawer
+    private var viewBounds: CGRect {
+        return CGRect(
+            origin: CGPoint(x: 0, y: 0),
+            size: bounds.size
+        )
+    }
+    
+    private func updateRange()
+    {
+        rangeMinimumX = -axesOrigin.x / scale
+        rangeMaximumX =  rangeMinimumX + (bounds.size.width / scale)
+        increment = (1.0/scale)
+        
+    }
+    
+    
+    override func drawRect(rect: CGRect)
+    {
+        let axesDrawer = AxesDrawer(
+            color: UIColor.blueColor() ,
+            contentScaleFactor: contentScaleFactor
+        )
+        
+        axesOrigin.x = center.x
+        axesOrigin.y = center.y
+        
+        axesDrawer.drawAxesInRect(
+            viewBounds,
+            origin: axesOrigin,
+            pointsPerUnit: scale
+        )
+    }
+    
 
 }
